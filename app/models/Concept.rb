94280eab43e2daba7adc2031067c7b2231eb0616
Brook <brookbeyene37@gmail.com>
2020-05-28 14:21:10 -0600

    seed, student, and concept changed

--- app/models/Concept.rb
+++ app/models/Concept.rb
@@ -6,10 +6,12 @@ class Concept < ActiveRecord::Base
         Concept.pluck(:data_structure).uniq
     end
 
-    def self.list_tasks_by_data_structure(data_structure)
-        list = Concept.list_unique_data_structures
-        if data_structure == list
-            return list[0][1]
-        end
-    end
+    
+
+    # def self.list_tasks_by_data_structure(data_structure)
+    #     list = Concept.list_unique_data_structures
+    #     if data_structure == list
+    #         return list[0][1]
+    #     end
+    # end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -15,6 +15,91 @@ c2 = Concept.create(
     task: "Reverse a binary tree's leaves",
     full_description: ""
 )
+Concept.create(
+    data_structure: "Trees",
+    task: "Find Maximum depth of an N-ary Tree",
+    full_description: "The maximum depth is the number of nodes along the longest path from the root node down to the farthest leaf node."
+)
+
+Concept.create(
+    data_structure: "Trees",
+    task: "Delete leaves with a given value from a binary tree",
+    full_description: "Note that once you delete a leaf node with value target, if it's parent node becomes a leaf node and has the value target, it should also be deleted (you need to continue doing that until you can't)."
+)
+
+
+Concept.create(
+    data_structure: "Hash Tables",
+    task: "Longest substring",
+    full_description: "Given a string, find the length of the longest substring without repeating characters."
+)
+
+
+Concept.create(
+    data_structure: "Hash Tables",
+    task: "Valid anagram",
+    full_description: "Given two strings s and t , write a function to determine if t is an anagram of s."
+)
+
+Concept.create(
+    data_structure: "Hash Tables",
+    task: "Find the intersection of two arrays",
+    full_description: "Given two arrays, write a function to compute their intersection."
+)
+
+Concept.create(
+    data_structure: "Arrays",
+    task: "'Chunk' an array",
+    full_description: "Given an array, 'chunk' it into an array of arrays of size x"
+)
+Concept.create(
+    data_structure: "Arrays",
+    task: "Find the median of two sorted arrays",
+    full_description: ""
+)
+
+Concept.create(
+    data_structure: "Arrays",
+    task: "Remove duplicates from a sorted array",
+    full_description: "Given a sorted array nums, remove the duplicates in-place such that each element appear only once and return the new length."
+)
+
+Concept.create(
+    data_structure: "Arrays",
+    task: "Rotate an array",
+    full_description: "Given an array, rotate the array to the right by k steps, where k is non-negative."
+)
+
+Concept.create(
+    data_structure: "Arrays",
+    task: "Find Pivot Index",
+    full_description: "Given an array of integers nums, write a method that returns the pivot index of this array.
+    We define the pivot index as the index where the sum of all the numbers to the left of the index is equal to the sum of all the numbers to the right of the index.
+    "
+)
+Concept.create(
+    data_structure: "Linked Lists",
+    task: "Merge two Sorted Lists",
+    full_description: ""
+)
+
+Concept.create(
+    data_structure: "Linked Lists",
+    task: "Reverse a linked list",
+    full_description: ""
+)
+Concept.create(
+    data_structure: "Linked Lists",
+    task: "Remove all elements from a linked list with value X",
+    full_description: ""
+)
+Concept.create(
+    data_structure: "Linked Lists",
+    task: "Find the middle of a linked list",
+    full_description: ""
+)
+
+
 
 l1 = Lesson.create(
     student_id: s1.id,
